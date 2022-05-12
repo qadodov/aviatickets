5b805653c54bed01f752913490603fd9bdb231fc
Ilkhom Dodov <dodlordisback@gmail.com>
2022-05-12 19:31:39 +0300

    Modified

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -3,6 +3,8 @@ package ru.netology.manager;
 import ru.netology.ticket.Ticket;
 import ru.netology.repository.Repository;
 
+import java.util.Comparator;
+
 public class TicketManager {
 
     private Repository repository;
@@ -16,6 +18,32 @@ public class TicketManager {
     }
 
     public Ticket[] findAll(String from, String to) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (matchesFrom(ticket, from)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Ticket[] finalResult = new Ticket[0];
+        for (Ticket ticket : result) {
+            if (matchesTo(ticket, to)) {
+                Ticket[] tmpSecond = new Ticket[finalResult.length + 1];
+                for (int i = 0; i < finalResult.length; i++) {
+                    tmpSecond[i] = finalResult[i];
+                }
+                tmpSecond[tmpSecond.length - 1] = ticket;
+                finalResult = tmpSecond;
+            }
+        }
+        return finalResult;
+    }
+
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.findAll()) {
             if (matchesFrom(ticket, from)) {
--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -3,8 +3,10 @@ package ru.netology.manager;
 import org.junit.jupiter.api.Test;
 import ru.netology.ticket.Ticket;
 import ru.netology.repository.Repository;
+import ru.netology.ticket.TicketByPriceAscComparator;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +15,7 @@ class TicketManagerTest {
     Ticket t1 = new Ticket(1, 1500, "DME", "GZP", 150);
     Ticket t2 = new Ticket(2, 5000, "DME", "EGO", 120);
     Ticket t3 = new Ticket(3, 2000, "AAQ", "GZP", 200);
-    Ticket t4 = new Ticket(4, 1000, "DME", "EGO", 120);
+    Ticket t4 = new Ticket(4, 1000, "DME", "EGO", 220);
 
     Repository repo = new Repository();
     TicketManager manager = new TicketManager(repo);
@@ -26,10 +28,9 @@ class TicketManagerTest {
         manager.add(t3);
         manager.add(t4);
 
-
-
         Ticket[] expected = {t2, t4};
         Ticket[] actual = manager.findAll("DME", "EGO");
+
 
         assertArrayEquals(expected, actual);
     }
@@ -48,16 +49,31 @@ class TicketManagerTest {
     @Test
     void shouldSortSelectedTickets() {
 
+        manager.add(t4);
+        manager.add(t1);
+        manager.add(t3);
+        manager.add(t2);
+
+
+        Ticket[] expected = {t4, t2};
+        Ticket[] actual = manager.findAll("DME", "EGO");
+
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSortSelectedTicketsByDuration() {
+
         manager.add(t2);
         manager.add(t1);
         manager.add(t3);
         manager.add(t4);
 
 
-        Ticket[] expected = {t4, t2};
-        Ticket[] actual = manager.findAll("DME", "EGO");
+        Ticket[] expected = {t2, t4};
+        Ticket[] actual = manager.findAll("DME", "EGO", Ticket::compareTo);
 
-        Arrays.sort(actual);
 
         assertArrayEquals(expected, actual);
     }
